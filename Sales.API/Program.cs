c9519cc64b9869d9b88575e2c679b41d6ba579f1
diegosepulveda <diegosepulveda262671@correo.itm.edu.co>
2023-02-15 15:55:44 -0500

    creación inicial de la base de datos

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,18 +1,21 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Sales.API.Data.DataContext>(x=>x.UseSqlServer("name=DockerConnection"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(); 
     app.UseSwaggerUI();
 }
 
--- Sales.Shared/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Sales.Shared;
-public class Class1
-{
-
-}
-
