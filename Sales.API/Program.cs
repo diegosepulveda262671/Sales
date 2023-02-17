1c460dcf89f973f89307c2ed3ad6bc5e80ab23d8
diegosepulveda <diegosepulveda262671@correo.itm.edu.co>
2023-02-16 21:44:26 -0500

    Begin countries API controller

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -25,5 +25,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x=>x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowed(origin=>true)
+.AllowCredentials());
+
 app.Run();
 
