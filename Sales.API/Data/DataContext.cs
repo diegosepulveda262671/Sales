6150970a137d5f9aaaf2b27c1aa9cf077ac7e58b
diegosepulveda <diegosepulveda262671@correo.itm.edu.co>
2023-03-01 11:52:34 -0500

    add tables States and Cities

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -11,12 +11,18 @@ namespace Sales.API.Data
 
 		}
 		public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 			modelBuilder.Entity<Country>().HasIndex(x=>x.Name).IsUnique();
+            modelBuilder.Entity<State>().HasIndex("CountryId","Name").IsUnique();
+            modelBuilder.Entity<City>().HasIndex("StateId","Name").IsUnique();
+
             modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
         }
     }
