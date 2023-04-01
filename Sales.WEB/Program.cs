ea521a1f73428b4081c7e9fcc090df86ac57a9f7
diegosepulveda <diegosepulveda262671@correo.itm.edu.co>
2023-04-01 17:23:59 -0500

    finish activity 4

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -7,6 +7,8 @@ using Sales.WEB;
 using Sales.WEB.Auth;
 using Sales.WEB.Repositories;
 
+    
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
